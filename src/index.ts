6b452a76457a40eab47f273b6a7a659fa26e288b
memanot <menemty@gmail.com>
2021-11-05 11:15:14 +0200

    .

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import fetch from 'node-fetch'
+
+type TDnsOpts = {
+  host: string
+  ip: string
+}
+
+type TBegetSettings = {
+  login: string
+  pass: string
+}
+
+export class BegetAPI {
+  private settings: TBegetSettings
+
+  constructor(s: TBegetSettings) {
+    this.settings = s
+  }
+
+  async changeDns({ host, ip }: TDnsOpts) {
+    const { login, pass } = this.settings
+
+    const inputData = encodeURI(
+      JSON.stringify({
+        fqdn: host,
+        records: {
+          A: [{ priority: 10, value: ip }],
+          MX: [
+            { priority: 10, value: 'mx1.beget.com' },
+            { priority: 20, value: 'mx2.beget.com' },
+          ],
+        },
+      })
+    )
+
+    console.log(
+      await (
+        await fetch(
+          `https://api.beget.com/api/dns/changeRecords?login=${login}&passwd=${pass}&input_format=json&output_format=json&input_data=${inputData}`,
+          {
+            method: 'GET',
+          }
+        )
+      ).text()
+    )
+  }
+}
